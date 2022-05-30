d3bdda0a5b769b8d36974d506ffc846337c2aa5f
reyesgarcia-ana <reyesgarzia.ana@gmail.com>
2022-05-30 19:03:36 +0200

    Iteration#3

--- script.js
+++ script.js
@@ -68,5 +68,21 @@ function alumnsPassed (array) {
 
 // alumnsPassed (alumns);
 
+// Iteration#3 ---------------------------------------------------
+// Usa un bucle forof para recorrer todos los destinos del array. Imprime en un 
+// console.log sus valores.
 
+const placesToTravel = [
+'Japon', 
+'Venecia', 
+'Murcia', 
+'Santander', 
+'Filipinas', 
+'Madagascar'
+];
+
+
+for (var city of placesToTravel) {
+  console.log(city);
+};
 
