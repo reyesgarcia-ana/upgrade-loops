03d9d98883a59a049100589ae51e6f00a2396cf8
reyesgarcia-ana <reyesgarzia.ana@gmail.com>
2022-05-31 17:46:08 +0200

    Iteration#5

--- script.js
+++ script.js
@@ -101,5 +101,28 @@ for (property in alien) {
   console.log(`${property}: ${alien[property]}`);
 };
 
+// Iteration#5 ---------------------------------------------------
+// Usa un bucle for para recorrer todos los destinos del array y elimina los elementos 
+//que tengan el id 11 y 40. Imprime en un console log el array. Puedes usar este array:
 
+const placesToTravel2 = [
+
+    {id: 5, name: 'Japan'}, 
+    {id: 11, name: 'Venecia'}, 
+    {id: 23, name: 'Murcia'}, 
+    {id: 40, name: 'Santander'}, 
+    {id: 44, name: 'Filipinas'}, 
+    {id: 59, name: 'Madagascar'},
+];
+
+
+for (let index = 0; index < placesToTravel2.length; index++) {
+    
+    if (placesToTravel2[index].id === 11 || placesToTravel2[index].id === 40) {
+
+        placesToTravel2.splice(index,1);
+    }  
+};
+
+console.log(placesToTravel2);
 
